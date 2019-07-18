80fc00264127e02cc8bc275e0ba438ca70e2b33b
jszutkowski <yaro89@o2.pl>
2019-07-18 22:14:55 +0200

    added acceptance test

--- tests/LoginCest.php
+++ tests/LoginCest.php
@@ -1,16 +1,16 @@
 <?php
 class LoginCest 
-{
-	public function _before(AcceptanceTester $I)
+{    
+    public function _before(AcceptanceTester $I)
     {
         $I->amOnPage('/');
     }
 
     public function loginSuccessfully(AcceptanceTester $I)
     {
-        // write a positive login test
+        $I->see('Welcome!');
     }
-
+    
     public function loginWithInvalidPassword(AcceptanceTester $I)
     {
         // write a negative login test
